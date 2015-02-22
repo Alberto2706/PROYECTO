e9620d604023eca60fac458ed387945ed302637a
Alberto Lopez <linch_flequi@hotmail.com>
2015-02-22 16:35:21 -0600

    agregando funcionalidad a la lista (agregar y eliminar)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,4 +3,13 @@
 angular.module('proyectoApp')
   .controller('MainCtrl', function ($scope) {
     $scope.tareas = ['Item1', 'Item2', 'Item3'];
+
+    $scope.addTarea = function(){
+      $scope.tareas.push($scope.tarea);
+      $scope.tarea="";
+    };
+
+    $scope.eliminarTarea = function(index){
+      $scope.tareas.splice(index,1);
+    };
   });
