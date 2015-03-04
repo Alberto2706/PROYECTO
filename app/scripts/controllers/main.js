f0828ca1512105abad6af5fc99bc742c5452db3c
Alberto Lopez <linch_flequi@hotmail.com>
2015-03-04 15:27:41 -0600

    Persistir datos en almacenamiento local con angular-local-storage

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,12 @@
 'use strict';
 
 angular.module('proyectoApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.tareas = [];
+  .controller('MainCtrl', function ($scope, localStorageService) {
+    var tareasEnAlmacen = localStorageService.get('tareas');
+    $scope.tareas = tareasEnAlmacen && tareasEnAlmacen.split('\n') || [];
+    $scope.$watch('tareas', function(){
+      localStorageService.add('tareas', $scope.tareas.join('\n'));
+    }, true);
 
     $scope.addTarea = function(){
       $scope.tareas.push($scope.tarea);
